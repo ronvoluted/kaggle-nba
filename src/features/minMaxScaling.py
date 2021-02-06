a92d78b50b1315c3a50d6bf2f7cff33cd2e209e3
Kai-Ping Wang <rudecat2000@iinet.net.au>
2021-02-06 18:01:50 +1100

    Added cross validation implementation

--- src/features/minMaxScaling.py
+++ src/features/minMaxScaling.py
@@ -3,12 +3,11 @@ import logging
 from sklearn.preprocessing import MinMaxScaler
 import joblib
 
-def scale(X_train, X_val, X_test):
+def scale(X_train, X_val):
     """ Use MinMaxScaler to scale training, valuation and test sets, and store them into ../data/processed/
         PARAMETERS:
             X_train: training set (array)
             X_val: valuation set (array)
-            X_test: test set (array)
     """
     logger = logging.getLogger(__name__)
     logger.info('Min-max scaling on training, valuation and test sets')
@@ -16,8 +15,5 @@ def scale(X_train, X_val, X_test):
     sc = MinMaxScaler().fit(X_train)
     X_train = sc.transform(X_train)
     X_val = sc.transform(X_val)
-    X_test = sc.transform(X_test)
 
-    joblib.dump(X_train, "../data/processed/X_train")
-    joblib.dump(X_val, "../data/processed/X_val")
-    joblib.dump(X_test, "../data/processed/X_test")
+    return sc, X_train, X_val
--- src/models/eval_model.py
+++ src/models/eval_model.py
@@ -1,28 +1,29 @@
 from sklearn.metrics import accuracy_score, confusion_matrix,roc_curve, roc_auc_score, precision_score, recall_score, precision_recall_curve, f1_score, plot_confusion_matrix
 import matplotlib.pyplot as plt
 
-def eval_model(classifier, X_train, y_train, X_val, y_val):
+def eval_model(classifier, X_train, y_train, X_val, y_val, show=True):
   '''
   classifier : Model to be evaluated
   X_train    : Features of training set
   y_train    : Target value of training set
   X_val      : Features of valuation set
   y_val      : Target valu of valuation set
-	
+  show       : Boolean - if the performance should be shown or not
   '''
 
   model = classifier.fit(X_train,y_train)
-  roc_score_training = get_performance(model, X_train, y_train, "Train")
-  roc_score_val = get_performance(model, X_val, y_val, "Validate")  
+  roc_score_training = get_performance(model, X_train, y_train, "Train", show)
+  roc_score_val = get_performance(model, X_val, y_val, "Validate", show)  
 
   return model, roc_score_training, roc_score_val
 
-def get_performance(mod, xvar, yvar, runtype):
+def get_performance(mod, xvar, yvar, runtype, show):
   '''
   mod     : Model to be evaluated
   xvar    : Feature columns usually x_train/x_test/x_val
   yvar    : Target Column corresponding y_train/y_test/y_val
   runtype : Hardcoded value to indicate run type Train/Test/Validate. This gets printed at the end of score and as title for confusion matrix
+  show    : Boolean - if the performance should be shown or not
 	
   Extension Scope 
   - Add more relavent scores 
@@ -33,7 +34,10 @@ def get_performance(mod, xvar, yvar, runtype):
   mod_pred_proba = mod.predict_proba(xvar)[:, 1]
 
   mod_roc_score = roc_auc_score(yvar, mod_pred_proba)
-  print('Accuracy Score: ',accuracy_score(yvar,mod_pred),' F1 Score ',f1_score(yvar,mod_pred),' Recall Score ', recall_score(yvar,mod_pred), ' R2 Score ',mod.score(xvar, yvar),' ROC_AUC_SCORE ', mod_roc_score,'(',runtype,')')
-  disp = plot_confusion_matrix(mod, xvar, yvar, cmap=plt.cm.Blues)
-  disp.ax_.set_title('Confusion matrix '+runtype)
+  
+  if show:
+    print('Accuracy Score: ',accuracy_score(yvar,mod_pred),' F1 Score ',f1_score(yvar,mod_pred),' Recall Score ', recall_score(yvar,mod_pred), ' R2 Score ',mod.score(xvar, yvar),' ROC_AUC_SCORE ', mod_roc_score,'(',runtype,')')
+    disp = plot_confusion_matrix(mod, xvar, yvar, cmap=plt.cm.Blues)
+    disp.ax_.set_title('Confusion matrix '+runtype)
+  
   return mod_roc_score
