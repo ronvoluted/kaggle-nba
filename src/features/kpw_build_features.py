340bcd79c2d8f1eb0236b9e19a614fdafd55795e
Kai-Ping Wang <26175292+rudecat@users.noreply.github.com>
2021-02-15 00:22:33 +1100

    kpw Assignment B (#14)
    Co-authored-by: Kai-Ping Wang <rudecat2000@iinet.net.au>

--- src/features/kpw_build_features.py
+++ src/features/kpw_build_features.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import pandas as pd
 
 def build(X):
     """Build Features:
@@ -18,29 +19,29 @@ def build(X):
     logger = logging.getLogger(__name__)
     logger.info('Build features')
 
-    X = X.drop(columns=['FG%','3P%','FT%'])
-
-    X['FG%'] = X.apply(lambda row: row['FGM'] / row['FGA'] if row['FGA'] > 0 else 0, axis = 1)
-    X['FGM'] = X.apply(lambda row: row['FGM'] if row['FGM'] <= row['FGA'] else row['FGA'], axis = 1)
-    X['3P%'] = X.apply(lambda row: row['3P Made'] / row['3PA'] if row['3PA'] > 0 else 0, axis = 1)
-    X['3P Made'] = X.apply(lambda row: row['3P Made'] if row['3P Made'] <= row['3PA'] else row['3PA'], axis = 1)
-    X['FT%'] = X.apply(lambda row: row['FTM'] / row['FTA'] if row['FTA'] > 0 else 0, axis = 1)
-    X['FTM'] = X.apply(lambda row: row['FTM'] if row['FTM'] <= row['FTA'] else row['FTA'], axis = 1)
+    X['binned_GP'] = pd.cut(X['GP'], 5, labels=[1,2,3,4,5])
+    # X['binned_FG%'] = pd.cut(X['FG%'], 5, labels=[2,1,0,-1,-2], ordered=False)
+    # X['binned_3P%'] = pd.cut(X['3P%'], 5, labels=[2,1,0,-1,-2])
+    # X['binned_FT%'] = pd.cut(X['FT%'], 5, labels=[2,1,0,-1,-2])
     # *_small features are created when the stat is in lower 25%
-    X['GP_small'] = X.apply(lambda row: 1 if row['GP'] < 51 else 0, axis = 1)
-    X['MIN_small'] = X.apply(lambda row: 1 if row['MIN'] <= 12 else 0, axis = 1)
-    X['PTS_small'] = X.apply(lambda row: 1 if row['PTS'] <= 4.2 else 0, axis = 1)
-    X['FTM_small'] = X.apply(lambda row: 1 if row['FTM'] <= 0.7 else 0, axis = 1)
-    X['OREB_small'] = X.apply(lambda row: 1 if row['OREB'] <= 0.5 else 0, axis = 1)
-    X['AST_small'] = X.apply(lambda row: 1 if row['AST'] <= 0.6 else 0, axis = 1)
-    X['BLK_small'] = X.apply(lambda row: 1 if row['BLK'] <= 0.1 else 0, axis = 1)
+    # X['GP_small'] = pd.cut(X['GP'], 5, labels=[2,1,0,-1,-2])
+    X['MIN_small'] = pd.cut(X['MIN'], 5, labels=[2,1,0,-1,-2])
+    X['PTS_small'] = pd.cut(X['PTS'], 5, labels=[2,1,0,-1,-2])
+    X['FTM_small'] = pd.cut(X['FTM'], 5, labels=[2,1,0,-1,-2])
+    X['OREB_small'] = pd.cut(X['OREB'], 5, labels=[2,1,0,-1,-2])
+    X['AST_small'] = pd.cut(X['AST'], 5, labels=[2,1,0,-1,-2])
+    X['BLK_small'] = pd.cut(X['BLK'], 5, labels=[2,1,0,-1,-2])
     # BadStats combines all *_small features and then average them to form an indication.
     # BadStats = 1 means a player performed poorly across the board
     # BadStats = 0 means a player performed not-poorly across the board
-    X['BadStats'] = X.apply(lambda row: (row['GP_small'] + row['MIN_small'] + row['PTS_small'] + row['FTM_small'] + row['OREB_small'] + row['AST_small'] + row['BLK_small']) / 7, axis = 1)
-    X['NotActive'] = X.apply(lambda row: 1 if (row['FGA']*row['3PA']*row['FTA'] == 0) else 0, axis = 1)
-    X['NotTeamPlayer'] = X.apply(lambda row: 1 if (row['AST']*row['BLK']*row['DREB'] == 0) else 0, axis = 1)
+    X['BadStats'] = X.apply(lambda row: (row['MIN_small'] + row['PTS_small'] + row['FTM_small'] + row['OREB_small'] + row['AST_small'] + row['BLK_small']) / 6, axis = 1)
+    # X['BadStats'] = X.apply(lambda row: (row['FG%_bad'] + row['3P%_bad'] + row['FT%_bad']) / 3, axis = 1)
+    # X['NotActive'] = X.apply(lambda row: 1 if (row['FGA']*row['3PA']*row['FTA'] == 0) else 0, axis = 1)
+    # X['NotTeamPlayer'] = X.apply(lambda row: 1 if (row['AST']*row['BLK']*row['DREB'] == 0) else 0, axis = 1)
+
     # X = X.drop(columns=['FGM','FGA','3P Made','3PA','FTA','DREB','REB','TOV','STL'])
-    X = X.drop(columns=['GP_small','MIN_small','PTS_small','FTM_small','OREB_small','AST_small','BLK_small'])
+    X = X.drop(columns=['MIN_small','PTS_small','FTM_small','OREB_small','AST_small','BLK_small'])
+    # X = X.drop(columns=['FG%','3P%','FT%','GP'])
+    # X = X.drop(columns=['FG%_bad','3P%_bad','FT%_bad','GP'])
 
     return X
